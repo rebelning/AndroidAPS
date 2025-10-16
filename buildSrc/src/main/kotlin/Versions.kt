c7fd0c2c74723218f32dbe9f4c46665b17632e1a
rebelning <shaxiaoan@gmail.com>
2025-10-16 11:43:12 +0800

    fix bugs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
 
     // const val appVersion = "3.3.1.3"
     const val appVersion = "4.3.1.3"
-    const val versionCode = 1567
+    const val versionCode = 1569
 
     const val ndkVersion = "21.1.6352462"
 
