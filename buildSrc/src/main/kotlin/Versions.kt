af1c95b77af1cd9e5bcb11e31c66fb47c7837fff
rebelning <shaxiaoan@gmail.com>
2025-10-20 10:53:20 +0800

    fix emebcta bugs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
 
     // const val appVersion = "3.3.1.3"
     const val appVersion = "4.3.1.3"
-    const val versionCode = 1570
+    const val versionCode = 1571
 
     const val ndkVersion = "21.1.6352462"
 
