b1080562c6ff89cdc59276e7a6602fb212ff329e
rebelning <shaxiaoan@gmail.com>
2025-11-03 15:26:58 +0800

    fix queue

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
 
     // const val appVersion = "3.3.1.3"
     const val appVersion = "4.3.1.3"
-    const val versionCode = 1572
+    const val versionCode = 1574
 
     const val ndkVersion = "21.1.6352462"
 
